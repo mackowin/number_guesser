d7adf1cb33ef647753d6b9229c2e8c196c716b87
Natalia Mackowiak <nat.mackowiak@gmail.com>
2023-01-06 14:39:12 -0800

    Add alternative solution

--- script.js
+++ script.js
@@ -2,22 +2,37 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
+// Write your code below:
+
+
+const verifyHumanInput = human => {
+  if(human < 0 || human > 9) {
+    window.alert('Only 0-9 numbers are valid');
+  }
+}
+
+
 const generateTarget = () => {
   return Math.floor(Math.random() * 10);
 }
 
-const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
-  const humanDifference = Math.abs(targetGuess - humanGuess)
-  const computerDifference = Math.abs(targetGuess - computerGuess)
-  return humanDifference <= computerDifference;
+const getAbsoluteDistance = (player, target) => {
+  return absoluteDistance = Math.abs(player - target);
 }
 
-const updateScore = winner => {
-  if (winner === 'human') {
-    humanScore++;
-  } else if (winner === 'computer') {
-    computerScore++;
+const compareGuesses = (human, computer, target) => {
+  verifyHumanInput(human);
+  if(getAbsoluteDistance(human, target) > getAbsoluteDistance(computer, target)) {
+    return false;
+  } else {
+    return true;
   }
 }
 
+const updateScore = winner => winner === 'human' ? humanScore++ : computerScore++;
+
 const advanceRound = () => currentRoundNumber++;
+
+//console.log(advanceRound());
+//console.log(updateScore('human'));
+//console.log(updateScore('computer'));
